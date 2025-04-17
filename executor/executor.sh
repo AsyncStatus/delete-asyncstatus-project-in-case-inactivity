8d29b63f209a23e7d123ccb77ebd3c570fd07a6e
Tobiasz Gleba <tobiasz.gleba24@gmail.com>
2025-04-17 21:21:31 +0200

    feat: check for new commits

--- executor/executor.sh
+++ executor/executor.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 echo "LOL"
+REPO_URL="https://github.com/AsyncStatus/asyncstatus.git"
+TODAY=$(date +%Y-%m-%d)
+
+check_repo_activity() {
+    if git ls-remote --exit-code "$REPO_URL" &>/dev/null; then
+        LAST_COMMIT_DATE=$(git ls-remote --refs "$REPO_URL" | awk '{print $2}' | xargs -I {} git log -1 --format=%ci {} | cut -d' ' -f1)
+        if [[ "$LAST_COMMIT_DATE" == "$TODAY" ]]; then
+            echo "true"
+        else
+            echo "false"
+        fi
+    else
+        echo "false"
+    fi
+}
+
+check_repo_activity
